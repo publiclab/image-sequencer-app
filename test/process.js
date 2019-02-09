2cc46aa7e0d807c386a2d8e317eb68ab367e40f0
Varun Gupta <varun.gupta1798@gmail.com>
2019-02-09 16:34:14 +0530

    Fix tests and travis
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- test/process.js
+++ test/process.js
@@ -6,20 +6,50 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../src');
 let should = chai.should();
-
+let path = require('path');
 
 chai.use(chaiHttp);
 
 /*
- * Test the /api/v1/process route
+ * Test the /api/v1/process route for network image
  */
 describe('/POST /ap1/v1/process', () => {
     it('should send status 200', (done) => {
         chai.request(server)
             .post('/api/v1/process')
-            .send({ url: '../Images/Mario.png', upload: false, sequence: 'invert{}' })
+            .send({ url: 'https://publiclab.org/system/images/photos/000/024/687/original/barataria.jpg', upload: false, sequence: 'invert{}' })
             .end((err, res) => {
                 res.should.have.status(200);
+                done();
+            });
+    });
+});
+
+/*
+ * Test the /api/v1/process route for local image
+ */
+describe('/POST /ap1/v1/process', () => {
+    it('should send status 200', (done) => {
+        chai.request(server)
+            .post('/api/v1/process')
+            .send({ url: path.join(__dirname, '../Images/Mario.png'), upload: false, sequence: 'invert{}' })
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            });
+    });
+});
+
+/*
+ * Test the /api/v1/ route
+ */
+describe('/POST /ap1/v1/', () => {
+    it('should send status 404', (done) => {
+        chai.request(server)
+            .post('/api/v1/')
+            .send({ url: '../Images/Mario.png', upload: false, sequence: 'invert{}' })
+            .end((err, res) => {
+                res.should.have.status(404);
                 done();
             });
     });
