ec3f475e0c3982913c336a2a053e1dc36a919475
tech4GT <varun.gupta1798@gmail.com>
2019-02-09 16:17:16 +0530

    trying network image
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- test/process.js
+++ test/process.js
@@ -17,7 +17,7 @@ describe('/POST /ap1/v1/process', () => {
     it('should send status 200', (done) => {
         chai.request(server)
             .post('/api/v1/process')
-            .send({ url: '../Images/Mario.png', upload: false, sequence: 'invert{}' })
+            .send({ url: 'https://publiclab.org/system/images/photos/000/024/687/original/barataria.jpg', upload: false, sequence: 'invert{}' })
             .end((err, res) => {
                 res.should.have.status(200);
                 done();
