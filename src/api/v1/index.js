5a9ea11b51d908dc87d1e8e420e714b3e3746e54
tech4GT <varun.gupta1798@gmail.com>
2019-02-10 22:36:31 +0530

    add get route
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -9,23 +9,39 @@ router.post("/process", (req, res) => {
         then we generate the ouput and return as data URI or
         upload it imgur and return the url of the image.
     */
-
     const img = req.body.url,
         sequence = req.body.sequence,
-        upload = req.body.upload === 'true',
-        redirect = req.body.redirect === 'true';
-    sequencer.loadImages(img, () => {
-        sequencer.importString(sequence);
-        sequencer.run(out => {
-            res.status(200);
-            redirect ? res.redirect(out) : res.send({ data: out });
-        });
+        upload = req.body.upload === 'true';
+    process(img, sequence, (out) => {
+        res.status(200).send({ data: out });
     });
+
 });
+
+router.get("/process", (req, res) => {
+
+    console.log(req.query)
+    const img = req.query.url,
+        sequence = req.query.sequence,
+        upload = req.query.upload === 'true';
+    process(img, sequence, (out) => {
+        res.send(`<html>
+                    <img src= "${out}">
+                    </html>`);
+    })
+})
 
 
 router.use("/", (req, res) => {
     res.sendStatus(404);
 });
+
+function process(img, sequence, cb) {
+
+    sequencer.loadImages(img, () => {
+        sequencer.importString(sequence);
+        sequencer.run(cb);
+    });
+}
 
 module.exports = router;
