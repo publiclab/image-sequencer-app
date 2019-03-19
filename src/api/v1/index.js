d4f481c5131994fd827d87f6f252e7a7345cbf26
tech4GT <varun.gupta1798@gmail.com>
2019-03-19 12:40:41 +0530

    now accepting multiple urls and processing linearly
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router(),
-    sequencer = require('image-sequencer')({ ui: false });
+    sequencer = require('image-sequencer');
 
 
 router.post("/process", (req, res) => {
@@ -20,15 +20,33 @@ router.post("/process", (req, res) => {
 
 router.get("/process", (req, res) => {
 
+    req.query.urls = JSON.parse(req.query.urls)
     console.log(req.query)
-    const img = req.query.url,
+    const imgs = req.query.urls,
         sequence = req.query.sequence,
         upload = req.query.upload === 'true';
-    process(img, sequence, (out) => {
-        res.send(`<html>
-                    <img src= "${out}">
-                    </html>`);
-    })
+
+    let i = 0
+    let cb = (out) => {
+        imgs[i] = out;
+        if (i < imgs.length - 1) {
+            console.log(i);
+            i++;
+            process(imgs[i], sequence, cb);
+        } else {
+
+            /* Here we want combine the images */
+
+            var html = `<html>`
+            for (let img of imgs) {
+                html += `<img src= "${img}">`
+            }
+            html += `</html>`
+            res.send(html);
+        }
+    }
+    process(imgs[0], sequence, cb)
+
 })
 
 
@@ -38,9 +56,10 @@ router.use("/", (req, res) => {
 
 function process(img, sequence, cb) {
 
-    sequencer.loadImages(img, () => {
-        sequencer.importString(sequence);
-        sequencer.run(cb);
+    const sequencerInstance = sequencer({ ui: false });
+    sequencerInstance.loadImages(img, () => {
+        sequencerInstance.importString(sequence);
+        sequencerInstance.run(cb);
     });
 }
 
