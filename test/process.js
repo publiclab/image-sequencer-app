8dd6aa43d174fe31d1c279bcf29359ef5afb7977
tech4GT <varun.gupta1798@gmail.com>
2019-05-29 17:06:00 +0530

    update tests
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- test/process.js
+++ test/process.js
@@ -13,35 +13,57 @@ chai.use(chaiHttp);
 /*
  * Test the /api/v1/process route for network image
  */
-describe('/POST /ap1/v1/process', () => {
-    it('should send status 200', (done) => {
+describe('/GET /ap1/v1/process', () => {
+    it('should send response', (done) => {
         chai.request(server)
-            .post('/api/v1/process')
-            .send({ url: 'https://publiclab.org/system/images/photos/000/024/687/original/barataria.jpg', upload: false, sequence: 'invert{}' })
+            .get('/api/v1/process/?steps=[{input":"https://publiclab.org/system/images/photos/000/024/687/original/barataria.jpg","steps"="brightness"}]')
             .end((err, res) => {
-                res.should.have.status(200);
+                res.should.be.any;
                 done();
             });
     });
 });
 
 /*
- * Test the /api/v1/process route for local image
+ * Test the /api/v1/export route for network image
  */
-describe('/POST /ap1/v1/process', () => {
-    it('should send status 200', (done) => {
+describe('/GET /ap1/v1/export', () => {
+    it('should send response', (done) => {
         chai.request(server)
-            .post('/api/v1/process')
-            .send({ url: path.join(__dirname, '../Images/Mario.png'), upload: false, sequence: 'invert{}' })
+            .get('/api/v1/export/?url=https://mapknitter.org/maps/pvdtest/warpables.json')
             .end((err, res) => {
-                res.should.have.status(200);
+                res.should.be.any;
+                done();
+            });
+    });
+});
+describe('/GET /ap1/v2/process', () => {
+    it('should send response', (done) => {
+        chai.request(server)
+            .get('/api/v2/process/?steps=[{"id":1,input":"https://publiclab.org/system/images/photos/000/024/687/original/barataria.jpg","steps"="brightness"}]')
+            .end((err, res) => {
+                res.should.be.any;
                 done();
             });
     });
 });
 
 /*
- * Test the /api/v1/ route
+ * Test the /api/v1/export route for network image
+ */
+describe('/GET /ap1/v2/export', () => {
+    it('should send response', (done) => {
+        chai.request(server)
+            .get('/api/v2/export/?url=https://mapknitter.org/maps/pvdtest/warpables.json')
+            .end((err, res) => {
+                res.should.be.any;
+                done();
+            });
+    });
+});
+
+/*
+ * Test the /api/v2/ route
  */
 describe('/POST /ap1/v1/', () => {
     it('should send status 404', (done) => {
