1256685ebe4e81e70cad5ad9ba1ffc70a8ebb69d
tech4GT <varun.gupta1798@gmail.com>
2019-06-15 20:53:30 +0530

    increase listeners
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
+require('events').EventEmitter.prototype.setMaxListeners(1000);
 const app = require('./index.js'),
     port = process.env.port || 4000;
-require('events').EventEmitter.prototype.setMaxListeners(100);
 app.listen(port, () => console.log(`Server started on port ${port}`));
