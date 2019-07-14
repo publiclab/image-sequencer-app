7cbc347a13dd8ea52a63e758109f69f83e72cd57
Varun Gupta <varun.gupta1798@gmail.com>
2019-07-14 19:23:58 +0530

    Update converter-multiSequencer.js

--- src/api/v2/util/converter-multiSequencer.js
+++ src/api/v2/util/converter-multiSequencer.js
@@ -1,13 +1,11 @@
-module.exports = function convert(arr) {
+module.exports = function convert(arr,scale) {
     let rv = []
     let id = 1;
-    let minX, maxX, minY, maxY, width = 0, height = 0;
+    let minX, maxX, minY, maxY;
     for (let obj of arr) {
-        if (obj["nodes"].length != 0) {
-            width += obj.width;
-            height += obj.height;
-        }
         for (let node of obj["nodes"]) {
+            node.lat = parseFloat(node.lat);
+            node.lon = parseFloat(node.lon);
             if (!minX || minX > node.lon) {
                 minX = node.lon;
             }
@@ -22,10 +20,7 @@ module.exports = function convert(arr) {
             }
         }
     }
-    while (width * height * 4 > Math.pow(2, 26)) {
-        width /= 2;
-        height /= 2;
-    }
+
     let dependsArray = [];
     let lMins = [];
     for (let obj of arr) {
@@ -34,9 +29,9 @@ module.exports = function convert(arr) {
         id++;
         for (let node of obj["nodes"]) {
             flag = true;
-            coords.push({ x: Math.round((node.lon - minX) * width / (maxX - minX)), y: Math.round((node.lat - minY) * height / (maxY - minY)) });
+            coords.push({ x: Math.round((node.lon - minX) * Math.cos(node.lat * Math.PI / 180) * 111 * 1000 * scale / (maxX - minX)), y: Math.round((node.lat - minY) * 1000 * 111 * scale / (maxY - minY)) });
         }
-        // console.log(coords)
+        console.log(coords)
         if (flag) {
             vals.steps = `webgl-distort{${encodeURIComponent(`nw:${coords[0].x}%2C${coords[0].y}|ne:${coords[1].x}%2C${coords[1].y}|se:${coords[2].x}%2C${coords[2].y}|sw:${coords[3].x}%2C${coords[3].y}`)}}`
             dependsArray.push(vals.id);
@@ -55,9 +50,8 @@ module.exports = function convert(arr) {
             id--;
         }
     }
-    // console.log(lMins)
     let vals = { id: id, input: rv[0].id, depends: dependsArray };
-    vals.steps = `canvas-resize{width:${2 * width}|height:${2 * height}|x:${lMins[0].x}|y:${lMins[0].y}}`;
+    vals.steps = `canvas-resize{width:${1000}|height:${1000}|x:${lMins[0].x}|y:${lMins[0].y}}`;
     for (let i in rv) {
         if (i == 0) continue;
         vals.steps += `,import-image{url:output>${rv[i].id}},overlay{x:${lMins[i].x}|y:${lMins[i].y}}`;
