779f3f8910b2444c3ffea459d09656285f205ebc
Jeffrey Warren <jeff@unterbahn.com>
2019-08-20 11:01:20 -0400

    Corner fix to dev branch (#25)
    * attempted fix
    
    * projection and fixes
    
    * using Leaflet port from https://github.com/Leaflet/Leaflet/blob/dc6a0ae61a70b1d34f9ee2c4f814bdd21841c774/src/geo/projection/Projection.SphericalMercator.js
    
    * revert to proj4
    
    * reordered to 3120, invert y

--- src/api/v2/util/converter-multiSequencer.js
+++ src/api/v2/util/converter-multiSequencer.js
@@ -1,19 +1,22 @@
-const hv = require('haversine')
+const proj = require('proj4')
+
 module.exports = function convert(arr, scale) {
     let rv = []
     let id = 1;
-    let minX, maxX, minY, maxY;
+
+    // find min/max coordinates
+    let minLon, maxX, minLat, maxY;
     for (let obj of arr) {
         obj.width = parseInt(obj.width)
         obj.height = parseInt(obj.height)
         for (let node of obj["nodes"]) {
             node.lat = parseFloat(node.lat);
             node.lon = parseFloat(node.lon);
-            if (!minX || minX > node.lon) {
-                minX = node.lon;
+            if (!minLon || minLon > node.lon) {
+                minLon = node.lon;
             }
-            if (!minY || minY > node.lat) {
-                minY = node.lat;
+            if (!minLat || minLat > node.lat) {
+                minLat = node.lat;
             }
             if (!maxX || maxX < node.lon) {
                 maxX = node.lon;
@@ -30,50 +33,47 @@ module.exports = function convert(arr, scale) {
         let vals = { id: id, input: obj.src, depends: [] }
         let flag = false, coords = []
         id++;
-        for (let it = 0; it < obj["nodes"].length; it++) {
-            let offset = 1
-            let node = obj["nodes"][(it + offset) % obj["nodes"].length]
-            flag = true;
-            let start = {
-                longitude: minX,
-                latitude: minY
-            }
-            let end = {
-                longitude: node.lon,
-                latitude: minY,
-            }
-            const minDistX = hv(start, end, { unit: 'meter' })
-            start = {
-                longitude: minX,
-                latitude: minY
-            }
-            end = {
-                longitude: minX,
-                latitude: node.lat
-            }
-            const minDistY = hv(start, end, { unit: 'meter' })
 
-            coords.push({ x: minDistX * scale, y: minDistY * scale });
-        }
-        console.log(coords)
-        if (flag) {
-            vals.steps = `webgl-distort{${encodeURIComponent(`nw:${coords[0].x}%2C${coords[0].y}|ne:${coords[1].x}%2C${coords[1].y}|se:${coords[2].x}%2C${coords[2].y}|sw:${coords[3].x}%2C${coords[3].y}`)}}`
-            dependsArray.push(vals.id);
-            let lminX, lminY;
-            for (let o of coords) {
-                if (lminX === undefined || lminX > o.x) {
-                    lminX = o.x;
-                }
-                if (lminY === undefined || lminY > o.y) {
-                    lminY = o.y;
-                }
+        if (obj.nodes.length > 0) {
+            // reorder from 0, 2, 1, 3 to 3, 1, 2, 0
+            let nodes = [
+                obj["nodes"][3],
+                obj["nodes"][2],
+                obj["nodes"][1],
+                obj["nodes"][0]
+            ];
+            let [minX, minY] = proj('WGS84','EPSG:900913', [minLon, minLat]);
+
+            // collect coordinates relative to minLon, minLat origin
+            for (let node of nodes) {
+                flag = true;
+                let coord = proj('WGS84','EPSG:900913', [node.lon, node.lat]);
+                coords.push({
+                    x: Math.round((coord[0] - minX) * scale),
+                    y: Math.round((coord[1] - minY) * scale) * -1
+                });
             }
-            lMins.push({ x: lminX, y: lminY });
-            rv.push(vals);
-        } else {
-            id--;
+            if (flag) {
+                vals.steps = `webgl-distort{${encodeURIComponent(`nw:${coords[0].x}%2C${coords[0].y}|ne:${coords[1].x}%2C${coords[1].y}|se:${coords[2].x}%2C${coords[2].y}|sw:${coords[3].x}%2C${coords[3].y}`)}}`
+ 
+                dependsArray.push(vals.id);
+                let lminLon, lminLat;
+                for (let o of coords) {
+                    if (lminLon === undefined || lminLon > o.x) {
+                        lminLon = o.x;
+                    }
+                    if (lminLat === undefined || lminLat > o.y) {
+                        lminLat = o.y;
+                    }
+                }
+                lMins.push({ x: lminLon, y: lminLat });
+                rv.push(vals);
+            } else {
+                id--;
+            }
         }
     }
+
     let vals = { id: id, input: rv[0].id, depends: dependsArray };
     vals.steps = `canvas-resize{width:${1000}|height:${1000}|x:${0}|y:${0}}`;
     for (let i in rv) {
