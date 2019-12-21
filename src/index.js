13bd227ccc74a990ef05309ec1b438830b9c78be
tech4GT <varun.gupta1798@gmail.com>
2019-12-21 11:33:09 +0530

    add cors middleware
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express'),
-    bp = require('body-parser');
+    bp = require('body-parser'),
+    cors = require('cors');
 
 const app = express(),
     api = require('./api');
 
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
+app.use(cors());
 
 
 // Mount the API router
